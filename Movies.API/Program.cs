e60f2d533380a8196cd916d057d2ae7fd7c95ddf
Benjamin Olumuyiwa <benjaminsqlserver@gmail.com>
2024-03-11 09:47:42 +0100

    End of program class implementation

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,25 +1,69 @@
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+//var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using Microsoft.OpenApi.Writers;
+using Movies.Infrastructure.Data;
+
+namespace Movies.API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host=CreateHostBuilder(args).Build();
+            await CreateAndSeedDb(host);
+            host.Run();
+        }
+
+        private static async Task CreateAndSeedDb(IHost host)
+        {
+            using(var scope=host.Services.CreateScope())
+            {
+                var services=scope.ServiceProvider;
+                var loggerFactory=services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var movieContext=services.GetRequiredService<MovieContext>();
+                    await MovieContextSeed.SeedAsync(movieContext, loggerFactory);
+
+                }
+                catch(Exception ex) 
+                {
+                    var logger=loggerFactory.CreateLogger<Program>();
+                    logger.LogError($"Exception Occurred In API: {ex.Message}");
+                }
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+         => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
